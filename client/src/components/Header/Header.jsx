1e20e98b817a26c1855594a91b90b7ced656428e
BilalHassen <bilalhassen097@gmail.com>
2024-01-06 15:30:15 -0500

    add a logo and desktop styling

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,47 @@
 import "./Header.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import React, { useState } from "react";
+import ecoStarLogo from "../../assets/logo/eco-star-logo.png";
 const Header = () => {
+  const [activePage, setActivePage] = useState("");
+
+  let isActive = (page) => {
+    setActivePage(page);
+  };
+
   return (
     <>
       <nav className="nav">
-        <div className="nav__logo-container">
+        <NavLink to="/" className="nav__logo-container">
           <h1 className="nav__logo">
             Eco<span>Star</span>
           </h1>
-        </div>
+          <div className="nav__logo-con">
+            <img
+              className="nav__logo-img"
+              src={ecoStarLogo}
+              alt="eco-star-logo"
+            ></img>
+          </div>
+        </NavLink>
         <div className="nav__links-container">
           <ul className="nav__list">
-            <li className="nav__list-item">LeaderBoard</li>
-            <li className="nav__list-item">Login</li>
+            <li className="nav__list-item">
+              <NavLink to="" className="nav__link">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav__list-item">
+              <NavLink to="" className="nav__link">
+                leaderboard
+              </NavLink>
+            </li>
+            <li className="nav__list-item">
+              <NavLink to="" className="nav__link">
+                login
+              </NavLink>
+            </li>
           </ul>
         </div>
       </nav>
