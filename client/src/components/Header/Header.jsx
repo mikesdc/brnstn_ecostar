9ff14ada4894ffe972d89149ecca2bef3c975d52
BilalHassen <bilalhassen097@gmail.com>
2024-01-06 13:20:59 -0500

    add styling for mobile

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import "./Header.scss";
-
+import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <>
+      <nav className="nav">
+        <div className="nav__logo-container">
+          <h1 className="nav__logo">
+            Eco<span>Star</span>
+          </h1>
+        </div>
+        <div className="nav__links-container">
+          <ul className="nav__list">
+            <li className="nav__list-item">LeaderBoard</li>
+            <li className="nav__list-item">Login</li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };
