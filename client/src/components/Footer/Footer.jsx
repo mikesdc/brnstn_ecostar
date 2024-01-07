dd1a57142e17b6c806c9fc99582f18bcdfd8cc00
BilalHassen <bilalhassen097@gmail.com>
2024-01-07 17:46:22 -0500

    add basic structure of footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import './Footer.scss';
-
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer__container">
-                <div className="footer__statement">
-                    
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <h1 className="footer__logo">
+        Eco<span>Star</span>
+      </h1>
+      <p className="footer__para">&copy; EcoStar All rights reserved</p>
+      <p className="footer__dev-header">Developed by:</p>
+      <div className="footer__list-container">
+        <ul className="footer__dev-list">
+          <li className="footer__list-item">Bilal Hassen</li>|
+          <li className="footer__list-item">Michael Sun Dela Cruz</li>|
+          <li className="footer__list-item">Aman Damani</li>|
+          <li className="footer__list-item">Arjun Sahjpaul</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
