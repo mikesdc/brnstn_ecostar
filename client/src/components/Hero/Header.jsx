6a55e6cff64cebfdf3b72a97a7d837dab3681149
BilalHassen <bilalhassen097@gmail.com>
2024-01-07 15:14:56 -0500

    add basic structure of hero component

--- client/src/components/Hero/Header.jsx
+++ client/src/components/Hero/Header.jsx
@@ -3,7 +3,19 @@ import "./Header.scss";
 const Header = () => {
   return (
     <>
-      <h1>Hero section</h1>
+      <div className="wrapper">
+        <section className="hero">
+          <div className="hero__container">
+            <h1 className="hero__title">
+              Take an active step in reducing CO2 and playing a role in
+              minimizing your carbon foot print by using EcoStar<span>©</span>
+            </h1>
+            <div className="hero__button-container">
+              <button className="hero__button">Join Now</button>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
