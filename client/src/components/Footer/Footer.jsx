2652beddc40e02d3b814750cad85703d962ed3fe
BilalHassen <bilalhassen097@gmail.com>
2024-01-07 17:57:51 -0500

    add in logo image and finish styling of footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Footer.scss";
-
+import ecoStarLogo from "../../assets/logo/eco-star-logo.png";
 const Footer = () => {
   return (
     <footer className="footer">
-      <h1 className="footer__logo">
-        Eco<span>Star</span>
-      </h1>
+      <div className="footer__logo-container">
+        <h1 className="footer__logo">
+          Eco<span>Star</span>
+        </h1>
+        <div className="footer__logo-con">
+          <img
+            className="footer__logo-img"
+            src={ecoStarLogo}
+            alt="eco-star-logo"
+          ></img>
+        </div>
+      </div>
       <p className="footer__para">&copy; EcoStar All rights reserved</p>
       <p className="footer__dev-header">Developed by:</p>
       <div className="footer__list-container">
