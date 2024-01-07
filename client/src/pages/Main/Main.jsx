671a2171f5cff6a43db686ab7dba39c2d54b2321
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-01-06 19:18:34 -0800

    Added Carbon Footprint Calculator functionality

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,5 +1,6 @@
 import "./Main.scss";
 import Header from "../../components/Header/Header";
+import Calculator from "../../components/Calculator/Calculator";
 import Footer from "../../components/Footer/Footer";
 
 const API_URL = process.env.REACT_APP_API_URL;
@@ -11,6 +12,7 @@ function Main() {
       <main className="main">
         <div className="main__card-example">Test Card</div>
       </main>
+      <Calculator />
       <Footer />
     </>
   );
