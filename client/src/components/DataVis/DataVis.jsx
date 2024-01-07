d2c94d6f18c72337d52663ba5182b1e1a6e95aef
Michael SDC <michael.sundelacruz@gmail.com>
2024-01-07 15:45:39 -0500

    update dev changes to this branch

--- client/src/components/DataVis/DataVis.jsx
+++ client/src/components/DataVis/DataVis.jsx
@@ -1,4 +1,5 @@
 import "./DataVis.scss";
+import { motion } from "framer-motion";
 
 const DataVis = ({ setShowJoinModal, totalCarbonSaved }) => {
   const handleJoinNow = () => {
@@ -15,14 +16,22 @@ const DataVis = ({ setShowJoinModal, totalCarbonSaved }) => {
           <div className="data-vis__header">
             <h1>EcoStars have saved </h1>
           </div>
-          <div className="data-vis__data">
-            <h1>{totalCarbonSaved}</h1>
-          </div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="data-vis__data">
+              <h1>{totalCarbonSaved || 0}</h1>
+            </div>
+          </motion.div>
 
           <div className="data-vis__header">
-            <h1>kilograms of CO<span className="subscript">2</span> </h1>
+            <h1>
+              kilograms of CO<span className="subscript">2</span>{" "}
+            </h1>
           </div>
-
         </div>
       </div>
     </main>
